86f388698fd76b6030644f0554bd5ac28f691cf9
Pouya-bit <Pouya_Ahrari@yahoo.com>
2025-04-04 17:09:16 -0600

    feat: add Catppuccin Frappe theme configuration for AstroNvim

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+      background = {
+        light = "latte",
+        dark = "frappe",
+      },
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "NONE" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  }
+} 
